ccea791decf87425ea1c6414a78a06e035708952
Murilo Braga <murilobraga3@gmail.com>
2024-09-27 18:43:58 -0300

    primeira commit

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,12 +1,36 @@
 document.addEventListener('DOMContentLoaded', function(){
-    document.getElementById('form-sort').addEventListener('submit', function(evento) {
+    document.getElementById('calc').addEventListener('submit', function(evento) {
         evento.preventDefault();
-        let numeroMaximo = document.getElementById('numerro-maximo').value;
-        numeroMaximo = parseInt(numeroMaximo);
+        let username = document.getElementById('name').value;
+        let peso = document.getElementById('peso').value;
+        peso = parseFloat(peso);
+        let alt = document.getElementById('alt').value;
+        alt = parseFloat(alt);
+        const valorIMC = (peso / (alt *alt)).toFixed(1);
+        let classificacao = '';
 
-        let numeroAleatorio = Math.random() * numeroMaximo;
+        if (valorIMC < 18.5){
+            classificacao = 'abaixo do peso.';
+        }
+        else if (valorIMC < 25){
+            classificacao = 'com peso ideal, Parabens!!!';
+        }
+        else if (valorIMC < 30){
+            classificacao = 'levemente acima do peso.';
+        }
+        else if (valorIMC < 35){
+            classificacao = 'com obesidade grau I';
+        }
+        else if (valorIMC < 40){
+            classificacao = 'com obesidade grau II';
+        }
+        else {
+            classificacao = 'com obesidade grau III';
+        }
 
-        document.getElementById('resultado-valor').innerText = Math.floor(numeroAleatorio + 1)
+        document.getElementById('username').innerText = username
+        document.getElementById('resultado-valor').innerText = valorIMC
         document.querySelector('.resultado').style.display = 'block';
+        document.getElementById('classificacao').innerText = classificacao
     })
 })
